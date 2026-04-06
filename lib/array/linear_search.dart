d4506376fb52298134d2f2b779f15d742dcbc166
Aayuska Adhikari <adhikariaayuska78@gmail.com>
2026-04-06 10:56:15 +0545

    LinearSearch

---
+++ lib/array/linear_search.dart
@@ -0,0 +1,25 @@
+// Linear Search in Dart
+int linearSearch(List<int> arr, int target) {
+  for (int i = 0; i < arr.length; i++) {
+    if (arr[i] == target) {
+      return i; // return index if found
+    }
+  }
+  return -1; // return -1 if not found
+}
+ 
+void main() {
+  List<int> numbers = [10, 20, 30, 40, 50];
+  int target = 30;
+ 
+  int result = linearSearch(numbers, target);
+ 
+  if (result != -1) {
+    print("Element $target found at index $result");
+  } else {
+    print("Element $target not found in the list");
+  }
+}
+ 
+ 
+ 
